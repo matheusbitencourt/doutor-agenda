9a7d55ad024feac8a3c9833891de8ab40ec722ea
matheusbitencourt <matheuslucas10@gmail.com>
2025-06-25 23:32:13 -0300

    feat: add clinic form page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,9 @@
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import { db } from "@/src/db";
+import { usersToClinicsTable } from "@/src/db/schema";
 import { auth } from "@/src/lib/auth";
 
 import SignOutButton from "./components/sign-out-button";
@@ -12,6 +15,14 @@ const DashboardPage = async () => {
 
   if (!session) {
     redirect("/authentication");
+  }
+
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+
+  if (clinics.length === 0) {
+    redirect("/clinic-form");
   }
 
   return (
